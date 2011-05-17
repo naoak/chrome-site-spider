41d85f9ac448afef8dd334695452093c62e35e85
neil.fraser@gmail.com <neil.fraser@gmail.com@b0001d72-e2ea-2af2-43dd-d86efe9f07a4>
2011-05-17 17:45:04 +0000

    Initial checkin
    git-svn-id: http://google-site-spider.googlecode.com/svn/trunk@2 b0001d72-e2ea-2af2-43dd-d86efe9f07a4

---
+++ background.js
@@ -0,0 +1,457 @@
+/**
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * How long to wait before one gives up on a connection.
+ * @type {number}
+ */
+var HTTP_REQUEST_TIMEOUT = 30 * 1000;
+/**
+ * Title of the results page (while spidering).
+ * @type {string}
+ */
+var RESULTS_TITLE = 'Site Spider Results';
+/**
+ * List of mime types that we will load for further spidering.
+ * text/plain is due to some web servers sending html using the wrong mime type.
+ * @type {Array.<string>}
+ */
+var SPIDER_MIME = ['text/html', 'text/plain', 'text/xml'];
+
+var popupDoc = null;
+var allowedText = '';
+var allowedRegex = null;
+var allowPlusOne = false;
+var allowArguments = false;
+var checkInline = false;
+var pagesTodo = {};
+var pagesDone = {};
+var spiderTab = null;
+var resultsTab = null;
+var httpRequest = null;
+var httpRequestWatchDogPid = 0;
+
+/**
+ * Save a reference to the popup's document object,
+ * then initialize the popup's fields.
+ * Called by the popup as soon as it is loaded.
+ * @param {Document} doc The popup's document object.
+ */
+function popupLoaded(doc) {
+  popupDoc = doc;
+  chrome.tabs.getSelected(null, setDefaultUrl_);
+}
+
+/**
+ * Initialize the popup's fields.
+ * Callback from chrome.tabs.getSelected.
+ * @param {Tab} The currently selected tab.
+ * @private
+ */
+function setDefaultUrl_(tab) {
+  // Use the currently selected tab's URL as a start point.
+  var url;
+  if (tab && tab.url && tab.url.match(/^\s*https?:\/\//i)) {
+    url = tab.url;
+  } else {
+    url = 'http://www.example.com/';
+  }
+  popupDoc.getElementById('start').value = url;
+
+  // Compute a default regex which will limit the spider
+  // to the current directory.
+  allowedText = url;
+  // Trim off any hash.
+  allowedText = trimAfter(allowedText, '#');
+  // Trim off any arguments.
+  allowedText = trimAfter(allowedText, '?');
+  // Trim off any filename, leaving the path.
+  var div = allowedText.lastIndexOf('/');
+  if (div > 'https://'.length) {
+    allowedText = allowedText.substring(0, div + 1);
+  }
+  // Sanitize regex characters in URL.
+  allowedText =
+      allowedText.replace(/([\^\$\.\*\+\?\=\!\:\|\\\(\)\[\]\{\}])/g,
+      '\\$1');
+  allowedText = '^' + allowedText;
+  popupDoc.getElementById('regex').value = allowedText;
+
+  // Restore previous setting for checkboxes.
+  popupDoc.getElementById('plusone').checked = allowPlusOne;
+  popupDoc.getElementById('arguments').checked = !allowArguments;
+  popupDoc.getElementById('inline').checked = checkInline;
+}
+
+/**
+ * Truncate a string to remove the specified character and anything after.
+ * e.g. trimAfter('ab-cd-ef', '-') -> 'ab'
+ * @param {string} string String to trim.
+ * @param {string} sep Character to split on.
+ * @return {string} String with character and any trailing substring removed.
+ */
+function trimAfter(string, sep) {
+  var div = string.indexOf(sep);
+  if (div != -1) {
+    return string.substring(0, div);
+  }
+  return string;
+}
+
+/**
+ * Start a spidering session.
+ * Called by the popup's Go button.
+ */
+function popupGo() {
+  // Terminate any previous execution.
+  popupStop();
+
+  // This plugin gets very unhappy when there are multiple concurrent results.
+  // Rename title of any previous results so we don't edit them.
+  var resultsWindows = chrome.extension.getViews({type: 'tab'});
+  for (var x = 0; x < resultsWindows.length; x++) {
+    var doc = resultsWindows[x].document;
+    if (doc.title == RESULTS_TITLE) {
+      doc.title = RESULTS_TITLE + ' - Closed';
+    }
+  }
+
+  // Attempt to parse the allowed URL regex.
+  var input = popupDoc.getElementById('regex');
+  allowedText = input.value;
+  try {
+    allowedRegex = new RegExp(allowedText);
+  } catch (e) {
+    alert('Restrict regex error:\n' + e);
+    popupStop();
+    return;
+  }
+
+  // Save settings for checkboxes.
+  allowPlusOne = popupDoc.getElementById('plusone').checked;
+  allowArguments = !popupDoc.getElementById('arguments').checked;
+  checkInline = popupDoc.getElementById('inline').checked;
+
+  // Initialize the todo and done lists.
+  pagesTodo = {};
+  pagesDone = {};
+  // Add the start page to the todo list.
+  var startPage = popupDoc.getElementById('start').value;
+  pagesTodo[startPage] = '[root page]';
+
+  /**
+   * Record a reference to the results tab so that output may be
+   * written there during spidering.
+   * @param {Tab} The new tab.
+   * @private
+   */
+  function resultsLoadCallback_(tab) {
+    resultsTab = tab;
+
+    var resultsDoc = getResultsDoc();
+    setInnerSafely(resultsDoc, 'startingOn', startPage);
+    setInnerSafely(resultsDoc, 'restrictTo', allowedText);
+
+    // Start spidering.
+    spiderPage();
+  }
+
+  // Open a tab for the results.
+  chrome.tabs.create({url: 'results.html'}, resultsLoadCallback_);
+}
+
+
+/**
+ * Set the innerHTML of a named element with a message.  Escape the message.
+ * @param {Document} doc Document containing the element.
+ * @param {string} id ID of element to change.
+ * @param {string} msg Message to set.
+ */
+function setInnerSafely(doc, id, msg) {
+  if (doc) {
+    var el = doc.getElementById(id);
+    if (el) {
+      msg = msg.replace(/&/g, '&amp;');
+      msg = msg.replace(/</g, '&lt;');
+      msg = msg.replace(/>/g, '&gt;');
+      el.innerHTML = msg;
+    }
+  }
+}
+
+/**
+ * Cleanup after a spidering session.
+ */
+function popupStop() {
+  pagesTodo = {};
+  spiderTab = null;
+  resultsTab = null;
+  window.clearTimeout(httpRequestWatchDogPid);
+  // Reenable the Go button.
+  popupDoc.getElementById('go').disabled = false;
+}
+
+/**
+ * Start spidering one page.
+ */
+function spiderPage() {
+  if (!resultsTab) {
+    // Results tab was closed.
+    return;
+  }
+
+  // Pull one page URL out of the todo list.
+  var url = null;
+  for (url in pagesTodo) {
+    break;
+  }
+  if (!url) {
+    // Done.
+    setStatus('Complete');
+    popupStop();
+    return;
+  }
+  var referrer = pagesTodo[url];
+  delete pagesTodo[url];
+  pagesDone[url] = true;
+
+  // Fetch this page using Ajax.
+  setStatus('Prefetching ' + url);
+  httpRequestWatchDogPid = window.setTimeout(httpRequestWatchDog,
+                                             HTTP_REQUEST_TIMEOUT);
+  httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = httpRequestChange;
+  httpRequest.open('HEAD', url, false);
+  httpRequest.url = url; // Create new 'url' property.
+  httpRequest.referrer = referrer; // Create new 'referrer' property.
+  // For some reason this request only works intermitently when called directly.
+  // Delay request by 1ms.
+  window.setTimeout('httpRequest.send(null)', 1);
+}
+
+/**
+ * Terminate an http request that hangs (happens when HTTP auth is used).
+ */
+function httpRequestWatchDog() {
+  setStatus('Aborting HTTP Request');
+  if (httpRequest) {
+    httpRequest.abort();
+    // Record page details.
+    recordPage(httpRequest.url, null, '[???]', httpRequest.referrer);
+    httpRequest = null;
+  }
+  window.setTimeout(spiderPage, 1);
+}
+
+/**
+ * Callback for when the status of the Ajax fetch changes.
+ */
+function httpRequestChange() {
+  if (!httpRequest || httpRequest.readyState < 2) {
+    // Still loading.  Wait for it.
+    return;
+  }
+  var code = httpRequest.status;
+  var mime = httpRequest.getResponseHeader('Content-Type') || '[none]';
+  var url = httpRequest.url;
+  var referrer = httpRequest.referrer;
+  httpRequest = null;
+  window.clearTimeout(httpRequestWatchDogPid);
+  setStatus('Prefetched ' + url + ' (' + code + ' ' + mime + ')');
+
+  // Record page details.
+  recordPage(url, code, mime, referrer);
+
+  // If this is a redirect or an HTML page, open it in a new tab and
+  // look for links to follow.  Otherwise, move on to next page.
+  if (url.match(allowedRegex) &&
+      ((code >= 300 && code < 400) ||
+      (code < 300 && SPIDER_MIME.indexOf(mime) != -1))) {
+    setStatus('Fetching ' + url);
+    chrome.tabs.create({url: url, selected: false}, spiderLoadCallback_);
+  } else {
+    setStatus('Next page...');
+    window.setTimeout(spiderPage, 1);
+  }
+}
+
+/**
+ * Inject the spider code into the newly opened page.
+ * @param {Tab} The new tab.
+ * @private
+ */
+function spiderLoadCallback_(tab) {
+  spiderTab = tab;
+  setStatus('Spidering ' + spiderTab.url);
+  chrome.tabs.executeScript(tab.id, {file: 'spider.js'});
+}
+
+// Add listener for message events from the injected spider code.
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+      if ('links' in request) {
+        spiderInjectCallback(request.links, request.inline);
+      }
+    });
+
+/**
+ * Process the data returned by the injected spider code.
+ * @param {Array} links List of links away from this page.
+ * @param {Array} inline List of inline resources in this page.
+ */
+function spiderInjectCallback(links, inline) {
+  var url = spiderTab.url;
+  setStatus('Scanning ' + url);
+  if (checkInline) {
+    links = links.concat(inline);
+  }
+  // Add any new links to the Todo list.
+  for (var x = 0; x < links.length; x++) {
+    var link = links[x];
+    link = trimAfter(link, '#');  // Trim off any hash.
+    if (!(link in pagesDone) && !(link in pagesTodo)) {
+      if (allowArguments || link.indexOf('?') == -1) {
+        if (link.match(allowedRegex) ||
+            (allowPlusOne && url.match(allowedRegex))) {
+          pagesTodo[link] = url;
+        }
+      }
+    }
+  }
+
+  // Close this page and mark done.
+  // In the case of a redirect this URL might be different than the one we
+  // marked spidered above.  Mark this one as spidered too.
+  pagesDone[url] = true;
+  chrome.tabs.remove(spiderTab.id);
+  spiderTab = null;
+  setStatus('Next page...');
+
+  // Move on to the next page.
+  window.setTimeout(spiderPage, 1);
+}
+
+/**
+ * Record the details of one url to the results tab.
+ * @param {string} url URL of page.
+ * @param {number} code HTTP status code of page.
+ * @param {string} mime MIME type of page.
+ */
+function recordPage(url, code, mime, referrer) {
+  url = '<a href="' + url + '" target="spiderpage" title="' + url + '">' +
+        url + '</a>';
+
+  // Mime often includes chartype:  text/html; charset=utf-8
+  var semicolon = mime.indexOf(';');
+  if (semicolon != -1) {
+    mime = mime.substring(0, semicolon);
+  }
+
+  var codeclass = '';
+  if (!code) {
+    codeclass = 'x0';
+  } else {
+    codeclass = 'x' + Math.floor(code / 100);
+  }
+  if (!code) {
+    code = 'Unable to load';
+  } else if (code in rfc2616) {
+    code += ' ' + rfc2616[code];
+  }
+
+  var resultsDoc = getResultsDoc();
+  if (resultsDoc) {
+    var tbody = resultsDoc.getElementById('resultbody');
+    var tr = resultsDoc.createElement('tr');
+    tr.innerHTML += '<td>' + url + '</td>' +
+                    '<td class="' + codeclass + '">' + code + '</td>' +
+                    '<td>' + mime + '</td>' +
+                    '<td><span title="' + referrer + '">' + referrer + '</span></td>';
+    tbody.appendChild(tr);
+  }
+}
+
+// HTTP Status Code Definitions.
+// http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+var rfc2616 = {
+  100: 'Continue',
+  101: 'Switching Protocols',
+  200: 'OK',
+  201: 'Created',
+  202: 'Accepted',
+  203: 'Non-Authoritative Information',
+  204: 'No Content',
+  205: 'Reset Content',
+  206: 'Partial Content',
+  300: 'Multiple Choices',
+  301: 'Moved Permanently',
+  302: 'Found',
+  303: 'See Other',
+  304: 'Not Modified',
+  305: 'Use Proxy',
+  306: '(Unused)',
+  307: 'Temporary Redirect',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  402: 'Payment Required',
+  403: 'Forbidden',
+  404: 'Not Found',
+  405: 'Method Not Allowed',
+  406: 'Not Acceptable',
+  407: 'Proxy Authentication Required',
+  408: 'Request Timeout',
+  409: 'Conflict',
+  410: 'Gone',
+  411: 'Length Required',
+  412: 'Precondition Failed',
+  413: 'Request Entity Too Large',
+  414: 'Request-URI Too Long',
+  415: 'Unsupported Media Type',
+  416: 'Requested Range Not Satisfiable',
+  417: 'Expectation Failed',
+  500: 'Internal Server Error',
+  501: 'Not Implemented',
+  502: 'Bad Gateway',
+  503: 'Service Unavailable',
+  504: 'Gateway Timeout',
+  505: 'HTTP Version Not Supported'
+};
+
+/**
+ * Set the current status message to the results tab.
+ * @param {string} msg Status message.
+ */
+function setStatus(msg) {
+  var resultsDoc = getResultsDoc();
+  setInnerSafely(resultsDoc, 'status', msg);
+}
+
+/**
+ * Get the document object of the results page.
+ * @return {Document} Result page's document object.
+ */
+function getResultsDoc() {
+  var resultsWindows = chrome.extension.getViews({type: 'tab'});
+  for (var x = 0; x < resultsWindows.length; x++) {
+    var doc = resultsWindows[x].document;
+    if (doc.title == RESULTS_TITLE) {
+      return doc;
+    }
+  }
+  // Someone closed the results?
+  popupStop();
+  return null;
+}
---
+++ tablesort.js
@@ -0,0 +1,307 @@
+/*
+Table Sorter v2.3
+Adds bi-directional sorting to table columns.
+Copyright 2005 Digital Routes, Scotland
+Copyright 2007 Neil Fraser, California
+Copyright 2011 Google Inc.
+http://neil.fraser.name/software/tablesort/
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Include on your page:
+  <SCRIPT LANGUAGE='JavaScript1.2' SRC='tablesort.js'></SCRIPT>
+*/
+
+// Namespace object.
+var TableSort = {};
+
+// Switch to enable or disable the TableSort.
+TableSort.enabled = true;
+
+// Default text values for the arrows.  Override these with custom image tags.
+TableSort.arrowNone = ' &nbsp;';
+TableSort.arrowUp   = ' &uarr;';
+TableSort.arrowDown = ' &darr;';
+
+// Tooltip to display when mousing over a sorting link.
+TableSort.titleText = 'Sort by this column';
+
+/**
+ * List of all the tables.
+ * @private
+ */
+TableSort.tables = [];
+
+/**
+ * Upon which column was the table sorted last time.  -=up, +=down
+ * @private
+ */
+TableSort.lastSort = [];
+
+
+/**
+ * Make all tables sortable.
+ */
+TableSort.initAll = function() {
+  if (!TableSort.enabled) {
+    return;
+  }
+  var tableNodeList = document.getElementsByTagName('TABLE');
+  for (var x = 0, table; table = tableNodeList[x]; x++) {
+    TableSort.initTable_(table);
+  }
+};
+
+
+/**
+ * Make one or more tables sortable.
+ * Call this function with the ID(s) of any tables which are created
+ * with DTHML after the page has loaded.
+ * @param {...string} var_args ID(s) of tables.
+ */
+TableSort.init = function(var_args) {
+  if (!TableSort.enabled) {
+    return;
+  }
+  for (var x = 0; x < arguments.length; x++) {
+    var table = document.getElementById(arguments[x]);
+    if (table) {
+      TableSort.initTable_(table);
+    }
+  }
+};
+
+
+/**
+ * Turn all the header/footer cells of one table into sorting links.
+ * @param {Element} table The table to be converted.
+ * @private
+ */
+TableSort.initTable_ = function(table) {
+  TableSort.tables.push(table);
+  var t = TableSort.tables.length - 1;
+  if (table.tHead) {
+    for (var y = 0, row; row = table.tHead.rows[y]; y++) {
+      for (var x = 0, cell; cell = row.cells[x]; x++) {
+        TableSort.linkCell_(cell, t, x);
+      }
+    }
+  }
+  if (table.tFoot) {
+    for (var y = 0, row; row = table.tFoot.rows[y]; y++) {
+      for (var x = 0, cell; cell = row.cells[x]; x++) {
+        TableSort.linkCell_(cell, t, x);
+      }
+    }
+  }
+  TableSort.lastSort[t] = 0;
+};
+
+
+/**
+ * Turn one header/footer cell into a sorting link.
+ * @param {!Element} cell The TH or TD to be made a link.
+ * @param {number} t Index of table in TableSort array.
+ * @param {number} x Column index.
+ * @private
+ */
+TableSort.linkCell_ = function(cell, t, x) {
+  if (TableSort.getClass_(cell)) {
+    var link = document.createElement('A');
+    link.href = 'javascript:TableSort.click(' + t + ', ' + x + ', "' +
+        escape(TableSort.getClass_(cell)) + '");';
+    if (TableSort.titleText) {
+      link.title = TableSort.titleText;
+    }
+    while(cell.hasChildNodes()) {
+      link.appendChild(cell.firstChild);
+    }
+    cell.appendChild(link);
+    // Add an element where the sorting arrows will go.
+    var arrow = document.createElement('SPAN');
+    arrow.innerHTML = TableSort.arrowNone;
+    arrow.className = 'TableSort_' + t + '_' + x;
+    cell.appendChild(arrow);
+  }
+};
+
+
+/**
+ * Return the class name for a cell.  The name must match a sorting function.
+ * @param {!Element} cell The cell element.
+ * @returns {string} Class name matching a sorting function.
+ * @private
+ */
+TableSort.getClass_ = function(cell) {
+  var className = (cell.className || '').toLowerCase();
+  var classList = className.split(/\s+/g);
+  for (var x = 0; x < classList.length; x++) {
+    if (('compare_' + classList[x]) in TableSort) {
+      return classList[x];
+    }
+  }
+  return '';
+};
+
+
+/**
+ * Sort the rows in this table by the specified column.
+ * @param {number} t Index of table in TableSort array.
+ * @param {number} column Index of the column to sort by.
+ * @param {string} mode Sorting mode (e.g. 'nocase').
+ */
+TableSort.click = function(t, column, mode) {
+  var table = TableSort.tables[t];
+  if (!mode.match(/^[_a-z0-9]+$/)) {
+    alert('Illegal sorting mode type.');
+    return;
+  }
+  var compareFunction = TableSort['compare_' + mode];
+  if (typeof compareFunction != 'function') {
+    alert('Unknown sorting mode: ' + mode);
+    return;
+  }
+  // Determine and record the direction.
+  var reverse = false;
+  if (Math.abs(TableSort.lastSort[t]) == column + 1) {
+    reverse = TableSort.lastSort[t] > 0;
+    TableSort.lastSort[t] *= -1;
+  } else {
+    TableSort.lastSort[t] = column + 1;
+  }
+  // Display the correct arrows on every header/footer cell.
+  var spanprefix1 = 'TableSort_' + t + '_';
+  var spanprefix2 = 'TableSort_' + t + '_' + column;
+  var spans = table.getElementsByTagName('SPAN');
+  for (var s = 0, span; span = spans[s]; s++) {
+    if (span.className && span.className.substring(0, spanprefix1.length) ==
+        spanprefix1) {
+      if (span.className.substring(0, spanprefix2.length) == spanprefix2) {
+        if (reverse) {
+          span.innerHTML = TableSort.arrowDown;
+        } else {
+          span.innerHTML = TableSort.arrowUp;
+        }
+      } else {
+        span.innerHTML = TableSort.arrowNone;
+      }
+    }
+  }
+  // Fetch the table's data and store it in a dictionary (assoc array).
+  if (!table.tBodies.length) {
+    return; // No data in table.
+  }
+  var tablebody = table.tBodies[0];
+  var cellDictionary = [];
+  for (var y = 0, row; row = tablebody.rows[y]; y++) {
+    var cell;
+    if (row.cells.length) {
+      cell = row.cells[column];
+    } else { // Dodge Safari 1.0.3 bug
+      cell = row.childNodes[column];
+    }
+    cellDictionary[y] = [TableSort.dom2txt_(cell), row];
+  }
+  // Sort the dictionary.
+  cellDictionary.sort(compareFunction);
+  // Rebuild the table with the new order.
+  for (y = 0; y < cellDictionary.length; y++) {
+    var i = reverse ? (cellDictionary.length - 1 - y) : y;
+    tablebody.appendChild(cellDictionary[i][1]);
+  }
+};
+
+
+/**
+ * Recursively build a plain-text version of a DOM structure.
+ * Bug: whitespace isn't always correct, but shouldn't matter for tablesort.
+ * @param {Element} obj Element to flatten into text.
+ * @returns {string} Plain-text contents of element.
+ * @private
+ */
+TableSort.dom2txt_ = function(obj) {
+  if (!obj) {
+    return '';
+  }
+  if (obj.nodeType == 3) {
+    return obj.data;
+  }
+  var textList = [];
+  for (var x = 0, child; child = obj.childNodes[x]; x++) {
+    textList[x] = TableSort.dom2txt_(child);
+  }
+  return textList.join('');
+};
+
+
+/**
+ * Case-sensitive sorting.
+ * Compare two dictionary structures and indicate which is larger.
+ * @param {Array} a First tuple.
+ * @param {Array} b Second tuple.
+ */
+TableSort.compare_case = function(a, b) {
+  if (a[0] == b[0]) {
+    return 0;
+  }
+  return (a[0] > b[0]) ? 1 : -1;
+};
+
+/**
+ * Case-insensitive sorting.
+ * Compare two dictionary structures and indicate which is larger.
+ * @param {Array} a First tuple.
+ * @param {Array} b Second tuple.
+ */
+TableSort.compare_nocase = function(a, b) {
+  var aLower = a[0].toLowerCase();
+  var bLower = b[0].toLowerCase();
+  if (aLower == bLower) {
+    return 0;
+  }
+  return (aLower > bLower) ? 1 : -1;
+};
+
+/**
+ * Numeric sorting.
+ * Compare two dictionary structures and indicate which is larger.
+ * @param {Array} a First tuple.
+ * @param {Array} b Second tuple.
+ */
+TableSort.compare_num = function(a, b) {
+  var aNum = parseFloat(a[0]);
+  if (isNaN(aNum)) {
+    aNum = -Number.MAX_VALUE;
+  }
+  var bNum = parseFloat(b[0]);
+  if (isNaN(bNum)) {
+    bNum = -Number.MAX_VALUE;
+  }
+  if (aNum == bNum) {
+    return 0;
+  }
+  return (aNum > bNum) ? 1 : -1;
+};
+
+
+if (window.addEventListener) {
+  window.addEventListener('load', TableSort.initAll, false);
+} else if (window.attachEvent) {
+  window.attachEvent('onload', TableSort.initAll);
+}
+
+if (navigator.appName == 'Microsoft Internet Explorer' &&
+    navigator.platform.indexOf('Mac') == 0) {
+  // The Mac version of MSIE is way too buggy to deal with.
+  TableSort.enabled = false;
+}
